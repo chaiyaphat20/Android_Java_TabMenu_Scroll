fa302ab4d2fd2dc7f5a91f5541088f014181026e
chaipat supharak <chaipat.sup@cpf.co.th>
2024-02-01 22:14:29 +0700

    first commit

---
+++ app/src/main/java/com/rudy/android_java_tab_menu_slide_more/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rudy.android_java_tab_menu_slide_more;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
