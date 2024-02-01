6479c51be1a5b564fb2573d259c326f895d81977
chaipat supharak <chaipat.sup@cpf.co.th>
2024-02-01 23:29:49 +0700

    feat:tab more

--- app/src/main/java/com/rudy/android_java_tab_menu_slide_more/MainActivity.java
+++ app/src/main/java/com/rudy/android_java_tab_menu_slide_more/MainActivity.java
@@ -1,14 +1,53 @@
 package com.rudy.android_java_tab_menu_slide_more;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager_id);
+        tabLayout = findViewById(R.id.tabLayout_id);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPagerAdapter.addFragment(new OneFragment(), "ข้อมูลโครงการ");
+        viewPagerAdapter.addFragment(new TwoFragment(), "เอกสารการขาย");
+        viewPagerAdapter.addFragment(new ThreeFragment(), "ประวัติเข้าเยี่ยม");
+        viewPagerAdapter.addFragment(new FourFragment(), "ติดตามโครงการ");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab !=null){
+                    viewPager.setCurrentItem(tab.getPosition());
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
